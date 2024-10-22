1c9c604f2e42433c1079c8e09ba10f56b094a73c
chauthutran <cthutran@gmail.com>
2024-10-22 13:45:41 +0900

    Updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Get()
+  async connectToDB() {
+    return { message: `Server is started !` };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -16,17 +16,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
-
-
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors(); // If needed
-//   const port = process.env.PORT || 3000;
-//   await app.listen(port);
-// }
-
-// bootstrap();
